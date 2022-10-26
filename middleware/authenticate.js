b2de9118c09640010174704c1f610579d905f015
musa1992 <mosesdev92@gmail.com>
2022-10-26 15:37:10 +0300

    add authorizePeopeManagement function

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -14,12 +14,33 @@ const authenticate = async (req,res,next)=>{
         req.user = decoded.user
         next()
     } catch (err) {
+        console.error(err.message)
         res.status(401).json({msg: 'Invalid token'})
     }
     
 }
 
+// allows only managers to create delete modify certain details of a user account
+const authorizePeopleManagement = async (req,res,next)=> {
+    try {
+        const user = await User.findById(req.user.id)
+        if(!user){
+            return res.status(400).json({msg: 'User not found'})
+        }
+
+        if(user.role !== 'Manager'){
+            return res.status(401).json({msg: 'Authorization denied.Access to this resource has been denied'})
+        }
+
+        next()
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+}
+
 module.exports = {
-    authenticate
+    authenticate,
+    authorizePeopleManagement
 }
 
