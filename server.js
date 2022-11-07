2ba48d2797f67e0cd66c6c48d10bd393d42dff92
musa1992 <mosesdev92@gmail.com>
2022-11-07 15:03:03 +0300

    ignore design folder

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const connectDB = require('./config/db')
 
+
 const app = express()
 connectDB()
 
 app.use(express.json({extended: false}))
+
 
 app.use('/api/users', require('./routes/UserRoutes'))
 app.use('/api/auth', require('./routes/AuthenticationRoute'))
 app.use('/api/project', require('./routes/ProjectRoutes'))
 app.use('/api/issue', require('./routes/IssueRoutes'))
 
+
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT} ...`)
+   
+    
 })
